ddb220af89efb603f67904ee666bb0dfcfd5edfc
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-14 18:54:02 +0300

    Initial Commit

---
+++ BucketList_crUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BucketList_crUD
+//
+//  Created by Atheer Alahmari on 10/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
